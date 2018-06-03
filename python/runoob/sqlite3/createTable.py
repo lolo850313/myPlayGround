a10aaa28655d269034a15d246525fc20ff2d7d68
lolo <1542441232@qq.com>
2018-06-04 00:22:12 +0800

    1

--- python/runoob/sqlite3/createTable.py
+++ python/runoob/sqlite3/createTable.py
@@ -1,1 +1,12 @@
-import 
+import sqlite3
+conn = sqlite3.connect('test.db')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE COMPANY
+            (ID INT PRIMARY KEY    NOT NULL,
+            NAME            TEXT   NOT NULL,
+            AGE             INT    NOT NULL,
+            ADDRESS         CHAR(50),
+            SALARY          REAL);''')
+
+conn.commit()
+conn.close()
