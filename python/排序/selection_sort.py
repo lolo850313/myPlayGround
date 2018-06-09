3af1f4bd946136d4d9021f0e80bb31226824e818
lolo <1542441232@qq.com>
2018-06-09 10:10:11 +0800

    vue

--- python/排序/selection_sort.py
+++ python/排序/selection_sort.py
@@ -5,6 +5,9 @@ def selection_sort(list):
         for j in range(i+1, length):
             if list[j] < list[min]:
                 min = j
+                print(list[min] , list[i])
                 list[min] , list[i] = list[i], list[min]
     
     return list
+
+selection_sort([3,2,4,1])
