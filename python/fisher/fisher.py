f9bfa1ff63fc79800a9c184f1d6f2ed42e27f5a8
lolo <1542441232@qq.com>
2018-07-11 00:38:33 +0800

    helper

--- python/fisher/fisher.py
+++ python/fisher/fisher.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+from helper import is_isbn_or_key
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/book/search')
-def search():
+@app.route('/book/search/<q>/<page>')
+def search(q,page):
+    is_isbn_or_key = is_isbn_or_key(q)
     pass
 
 if __name__ == '__main__':
