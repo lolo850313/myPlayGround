a292b6ef01c0181ef9dc52c49da3f95909c066c8
lolo <1542441232@qq.com>
2018-07-11 00:20:21 +0800

    1

--- python/fisher/fisher.py
+++ python/fisher/fisher.py
@@ -3,13 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/hello')
-def hello():
-    headers = {
-        'content-type' : 'application/json',
-        'location' : 'http://www.bing.com'
-    }
-    return 'hello 22',301,headers
+@app.route('/book/search')
+def search():
+    pass
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=app.config['DEBUG'],port=81)
