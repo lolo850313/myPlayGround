8a15c4ae2fa2a0a78569e631c4d9787e850887d5
lolo <1542441232@qq.com>
2018-07-12 23:24:00 +0800

    1

--- python/fisher/yushu_book.py
+++ python/fisher/yushu_book.py
@@ -1,8 +1,15 @@
+from http import HTTP
+
 class YuShuBook:
     isbn_url = "http://t.yushu.im/v2/book/isbn/{}"
     keyword_url = "http://t.yushu.im/v2/book/search?q={}&count{}&start={}"
+
     def search_by_isbn(self, isbn):
-        pass
+        url = YuShuBook.isbn_url.format(isbn)
+        result = HTTP.get(url)
+        return result
 
     def search_by_keyword(self, keyword):
-        pass
+        url = YuShuBook.isbn_url.format(keyword)
+        result = HTTP.get(url)
+        return result
