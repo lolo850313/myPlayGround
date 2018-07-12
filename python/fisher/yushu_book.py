144854609227696804aad3f5448eed2224911bfb
lolo <1542441232@qq.com>
2018-07-12 23:11:38 +0800

    1

--- python/fisher/yushu_book.py
+++ python/fisher/yushu_book.py
@@ -1,6 +1,8 @@
 class YuShuBook:
-    def search_by_isbn(self,url):
+    isbn_url = "http://t.yushu.im/v2/book/isbn/{}"
+    keyword_url = "http://t.yushu.im/v2/book/search?q={}&count{}&start={}"
+    def search_by_isbn(self, isbn):
         pass
 
-    def search_by_keyword(self,url):
+    def search_by_keyword(self, keyword):
         pass
