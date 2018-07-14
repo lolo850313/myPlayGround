1ef3a136af91e339b8edd6567de4977c3f2e1bbc
lolo <1542441232@qq.com>
2018-07-15 00:07:37 +0800

    1

--- python/fisher/fisher.py
+++ python/fisher/fisher.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from helper import is_isbn_or_key
+from yushu_book import YuShuBook
+# import json
+from flask import jsonify 
+
 app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route('/book/search/<q>/<page>')
 def search(q,page):
     isbn_or_key = is_isbn_or_key(q)
-    pass
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+    # return json.dumps(result), 200, {'content-type' : 'application/json'}
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=app.config['DEBUG'],port=81)
--- python/fisher/yushu_book.py
+++ python/fisher/yushu_book.py
@@ -4,12 +4,14 @@ class YuShuBook:
     isbn_url = "http://t.yushu.im/v2/book/isbn/{}"
     keyword_url = "http://t.yushu.im/v2/book/search?q={}&count{}&start={}"
 
-    def search_by_isbn(self, isbn):
-        url = YuShuBook.isbn_url.format(isbn)
+    @classmethod
+    def search_by_isbn(cls, isbn):
+        url = cls.isbn_url.format(isbn)
         result = HTTP.get(url)
         return result
 
-    def search_by_keyword(self, keyword):
-        url = YuShuBook.isbn_url.format(keyword)
+    @classmethod
+    def search_by_keyword(cls, keyword, count = 15, start = 0):
+        url = cls.isbn_url.format(keyword, count, start)
         result = HTTP.get(url)
         return result
