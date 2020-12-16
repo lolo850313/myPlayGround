4481e9b325af0337303845ada98375b6323b5177
lolo <1542441232@qq.com>
2020-12-16 18:19:53 +0800

    123

---
+++ javascript/react/reactTest/my-app/src/AutoFocusInput .js
@@ -0,0 +1,20 @@
+import React ,{ Component }from 'react';
+
+class AutoFocusInput  extends Component {
+	componentDidMount() {
+		this.input.focus()
+	}
+	render() {
+		return (
+			<div className="comment">
+				<div className="comment-user">
+					<span>{this.props.comment.username} : </span>
+				</div>
+				<p>{this.props.comment.content}</p>
+			</div>
+		)
+	}
+}
+
+
+export default AutoFocusInput 
