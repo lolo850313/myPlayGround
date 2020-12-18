d3b8caf8f3356acf055c86626ec4b945d3a9fff6
lolo <1542441232@qq.com>
2020-12-18 16:28:22 +0800

    123

--- javascript/react/reactTest/my-app/src/index.js
+++ javascript/react/reactTest/my-app/src/index.js
@@ -1,39 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentApp from './CommentApp'
 import './index.css';
 
-class Card extends Component {
-	render(){
-		return (
-			<div className="card">
-				//整个children引用
-				<div className="card-content">
-					{this.props.children}
-				</div>
-
-				//以数组形式单个引用
-				<div className="card-content">
-					{this.props.children[4]}
-				</div>
-				<div className="card-content">
-					{this.props.children[3]}
-				</div>
-				<div className="card-content">
-					{this.props.children[2]}
-				</div>
-			</div>
-		)
-	}
-
-}
-
-
 ReactDOM.render(
-		<Card>
-			<h2>childern1</h2>
-			<div>childern2</div>
-			childern3<input value="children4"/>
-			childern5
-		</Card> ,
+		<CommentApp />,
 	document.getElementById('root')
 );
