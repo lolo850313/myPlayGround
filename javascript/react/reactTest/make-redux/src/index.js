0e3196b11914e01634bde9ba16725f22e5260e02
lolo <1542441232@qq.com>
2021-01-05 11:17:28 +0800

    123

--- javascript/react/reactTest/make-redux/src/index.js
+++ javascript/react/reactTest/make-redux/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const appState = {
+	title : {
+		text : "React.js 小书",
+		color : "red"
+	},
+	content : {
+		text : "React.js 小书内容",
+		color : "blue"
+	}	
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function renderApp(appState) {
+	renderTitle(appState.title)
+	renderContent(appState.content)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function renderTitle(title) {
+	const titleDOM = document.getElementById("title")
+	titleDOM.innerHTML = title.text
+	titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+	const contentDOM = document.getElementById("content")
+	contentDOM.innerHTML = content.text
+	contentDOM.style.color = content.color
+}
+
+renderApp(appState)
