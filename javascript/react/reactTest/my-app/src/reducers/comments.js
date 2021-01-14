3526b1bef5f388be6f0cca3bfeecfaa64349618e
lolo <1542441232@qq.com>
2021-01-14 16:38:51 +0800

    123

---
+++ javascript/react/reactTest/my-app/src/reducers/comments.js
@@ -0,0 +1,41 @@
+//action type
+const INIT_COMMENTS = 'INIT_COMMENTS'
+const ADD_COMMENT = 'ADD_COMMENT'
+const DELETE_COMMENT = 'DELETE_COMMENT'
+
+//reducer
+export default (state, action) => {
+    if (!state) {
+        state = { comments : [] }
+    }
+
+    switch (action.type) {
+        case INIT_COMMENTS:
+            return { comments : action.comment}
+        case ADD_COMMENTS:
+            return { 
+                comments : [...state.comments, action.comment]
+            }
+        case DELETE_COMMENTS:
+            return { 
+                comments : [...state.comments.slice(0, action.commentIndex), 
+                    ...state.comments.slice(action.commentIndex + 1),
+                ]
+            }
+    
+        default:
+            return state;
+    }
+}
+
+export const initComments = (comments) => {
+    return { type: INIT_COMMENTS, comments}
+}
+
+export const addComment = (commentIndex) => {
+    return { type : ADD_COMMENT, commentIndex}
+}
+
+export const deleteComment = (commentIndex) => {
+    return { type : DELETE_COMMENT, commentIndex}
+}
