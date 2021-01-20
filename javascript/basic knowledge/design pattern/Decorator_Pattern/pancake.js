7ece0428c33a4c6be6ae6b0565a56e19e8198cc3
lolo <1542441232@qq.com>
2021-01-20 18:11:14 +0800

    123

--- javascript/basic knowledge/design pattern/Decorator_Pattern/pancake.js
+++ javascript/basic knowledge/design pattern/Decorator_Pattern/pancake.js
@@ -4,7 +4,7 @@ class Pancake {
     }
 
     getName() {
-        return this.name()
+        return this.name
     }
 
     getPrice() {
@@ -13,16 +13,61 @@ class Pancake {
     
 }
 
+// 装饰器
 class PancakeDecorator {
     constructor(pancake) {
-        this.Pancake = pancake
+        this.pancake = pancake
     }
     
-    getName {
+    getName (){
         return `${this.pancake.getName()}`
     }
 
-    getPrice {
+    getPrice (){
         return this.pancake.getPrice()
     }
 }
+
+//加鸡蛋
+class PancakeDecoratorWithEgg extends PancakeDecorator {
+    getName() {
+        return `${this.pancake.getName()} ➕鸡蛋`
+    }
+
+    getPrice() {
+        return this.pancake.getPrice() + 2
+    }
+}
+
+//加香肠
+class PancakeDecoratorWithSausage extends PancakeDecorator {
+    getName() {
+        return `${this.pancake.getName()} ➕香肠`
+    }
+
+    getPrice() {
+        return this.pancake.getPrice() + 1.5
+    }
+}
+
+//加培根
+class PancakeDecoratorWithBacon extends PancakeDecorator {
+    getName() {
+        return `${this.pancake.getName()} ➕培根`
+    }
+
+    getPrice() {
+        return this.pancake.getPrice() + 3
+    }
+}
+
+let pancake = new Pancake()
+
+pancake = new PancakeDecoratorWithEgg(pancake)
+console.log(pancake.getName(), pancake.getPrice())
+
+pancake = new PancakeDecoratorWithSausage(pancake)
+console.log(pancake.getName(), pancake.getPrice())
+
+pancake = new PancakeDecoratorWithBacon(pancake)
+console.log(pancake.getName(), pancake.getPrice())
