28a94090a6f0eb29f3e6b1f366d5821f403c65d9
lolo <1542441232@qq.com>
2021-01-28 16:50:28 +0800

    123

--- python/selenium/learn/test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-wd = webdriver.Chrome(r'C:\chromedriver.exe')
-
-wd.get("http://cdn1.python3.vip/files/selenium/sample1.html")
-a = wd.find_elements_by_class_name('animal')
-
-for i in a:
-    print(i.text)
-# search_input.send_keys("hewenhao")
