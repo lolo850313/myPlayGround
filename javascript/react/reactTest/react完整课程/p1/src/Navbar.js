dac70eb6e0d1cb1eec5388a747f3dffa2505bd07
lolo <1542441232@qq.com>
2021-01-28 21:16:49 +0800

    123

--- javascript/react/reactTest/react完整课程/p1/src/Navbar.js
+++ javascript/react/reactTest/react完整课程/p1/src/Navbar.js
@@ -1,5 +1,17 @@
-const  = () => {
-    return (  );
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Do jo Blog</h1>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/create" style={{ 
+                    color : "while",
+                    backgroundColor : "#f1356d",
+                    borderRadius:'8px'
+                }}>New blog</a>
+            </div>
+        </nav>
+     );
 }
  
-export default ;
+export default Navbar;
