684238a7a69f5541f89fbd64cacfa58b0e37f1bd
lolo <1542441232@qq.com>
2021-04-12 22:58:31 +0800

    123

--- javascript/openui5/walkthrough/w14/webapp/Component.js
+++ javascript/openui5/walkthrough/w14/webapp/Component.js
@@ -1,13 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/json/JSONModel"
 ], function (UIComponent, JSONModel) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
 
 		metadata : {
-			manifest : "json"
+			manifest: "json"
 		},
 
 		init : function () {
@@ -17,7 +17,7 @@ sap.ui.define([
 			// set data model
 			var oData = {
 				recipient : {
-					name : "World 11"
+					name : "World 12"
 				}
 			};
 			var oModel = new JSONModel(oData);
