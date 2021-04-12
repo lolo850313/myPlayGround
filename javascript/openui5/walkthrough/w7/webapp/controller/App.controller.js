de86255332920894f2f2e4ce7ddad3ce9efdd76f
lolo <1542441232@qq.com>
2021-04-12 15:02:14 +0800

    w8

--- javascript/openui5/walkthrough/w7/webapp/controller/App.controller.js
+++ javascript/openui5/walkthrough/w7/webapp/controller/App.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     'use strict';
     
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        
+        init : function () {
+            var oData = {
+                recipient : {
+                    name : "world"
+                }
+            }
+
+            var oModel = new JSONModel(oData)
+            this.getView().setModel(oModel)
+        },
+        
+        
         onShowHello : function () {
-            MessageToast.show("hello world 6")
+            MessageToast.show("hello world 7")
         }
     })
 });
