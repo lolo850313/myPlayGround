9a470d9879c4af49b575c3fb7217dcd4a69e4e91
lolo <1542441232@qq.com>
2021-04-15 22:42:01 +0800

    123

---
+++ javascript/openui5/walkthrough/w28/webapp/test/unit/model/formatter.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    'sap/ui/demo/walkthrough/model/formatter',
+    "sap/ui/model/resource/ResourceModel"
+], function(formatter, ResourceModel) {
+    'use strict';
+    
+    QUnit.module("Formatting functions", {
+        beforeEach : function () {
+            this._oResourceModel = new ResourceModel({
+                bundleUrl : sap.ui.require.toUrl("sap/ui/demo/walkthrough") + "/i18n/i18n.properties"
+            })
+        },
+
+        afterEach : function () {
+            this._oResourceModel.destroy()
+        }
+    })
+
+    QUnit.test("Should return the translate texts", function (assert) {
+        var oModel = this.stub()
+
+        oModel.withArgs("i18n").returns(this._oResourceModel)
+
+        var oViewStub = {
+            getModel : oModel
+        }
+
+        oModel.withArgs("i18n").returns(oViewStub)
+
+        var oControllerStub = {
+            getView : this.stub().returns(oViewStub)
+        }
+        var fnIsolatedFormatter = formatter.statusText.bind(oControllerStub)
+
+        assert.strictEqual(fnIsolatedFormatter("A"), "New", "The long text for status A is correct")
+
+        assert.strictEqual(fnIsolatedFormatter("B"), "In progress", "The long text for status B is correct")
+
+        assert.strictEqual(fnIsolatedFormatter("C"), "Done", "The long text for status C is correct")
+
+        assert.strictEqual(fnIsolatedFormatter("Foo"), "Foo", "The long text for status Foo is correct")
+    })
+});
