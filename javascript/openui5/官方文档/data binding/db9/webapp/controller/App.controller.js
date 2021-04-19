d63b5f03785a571543f2435507d2397071a80c29
lolo <1542441232@qq.com>
2021-04-19 21:32:33 +0800

    db9

---
+++ javascript/openui5/官方文档/data binding/db9/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/m/Library'
+], function(Controller, Library) {
+    'use strict';
+    
+    return Controller.extend("sap.ui.demo.db.controller.App", {
+        formatMail : function (sFirstName, sLastName) {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle()
+
+            return mobil
+        }
+    })
+});
