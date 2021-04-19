9d8d4a86ee4497b4b431929dedcc272fb4b177cc
lolo <1542441232@qq.com>
2021-04-19 13:54:25 +0800

    123

---
+++ javascript/openui5/官方文档/data binding/db6/webapp/index.js
@@ -0,0 +1,24 @@
+sap.ui.require([
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/model/BindingMode"
+], function (JSONModel, XMLView, BindingMode) {
+    "use strict";
+
+    sap.ui.getCore().attachInit(function () {
+        var oModel = new JSONModel({
+            firstName : "Harry",
+            lastName : "Hawk",
+            enabled: true,
+            panelHeaderText : "Data Binding Basics"
+        })
+
+        oModel.setDefaultBindingMode(BindingMode.OneWay)
+
+        sap.ui.getCore().setModel(oModel)
+
+        new XMLView({
+            viewName : "sap.ui.demo.db.view.App"
+        }).placeAt("content")
+    })
+})
