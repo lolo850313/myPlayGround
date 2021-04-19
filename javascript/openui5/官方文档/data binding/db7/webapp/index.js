b22809c2428d7b25204a1d5f72a251170a39baa1
lolo <1542441232@qq.com>
2021-04-19 15:14:18 +0800

    db8

---
+++ javascript/openui5/官方文档/data binding/db7/webapp/index.js
@@ -0,0 +1,28 @@
+sap.ui.require([
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
+    "use strict";
+
+    sap.ui.getCore().attachInit(function () {
+        var oModel = new JSONModel({
+            firstName : "Harry",
+            lastName : "Hawk",
+            enabled: true
+        })
+
+        sap.ui.getCore().setModel(oModel)
+
+        var oResourceModel = new ResourceModel({
+            bundleName : "sap.ui.demo.db.i18n.i18n",
+            supportedLocales : ["", "de"],
+            fallbackLocale : ""
+        })
+
+        sap.ui.getCore().setModel(oResourceModel, "i18n")
+        new XMLView({
+            viewName : "sap.ui.demo.db.view.App"
+        }).placeAt("content")
+    })
+})
