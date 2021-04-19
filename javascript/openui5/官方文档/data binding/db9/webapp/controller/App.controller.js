d30230b46f3db2f035cac256aa8848a949f995ba
lolo <1542441232@qq.com>
2021-04-19 21:54:49 +0800

    db9

--- javascript/openui5/官方文档/data binding/db9/webapp/controller/App.controller.js
+++ javascript/openui5/官方文档/data binding/db9/webapp/controller/App.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/Library'
-], function(Controller, Library) {
+    'sap/m/library'
+], function(Controller, mobileLibrary) {
     'use strict';
     
     return Controller.extend("sap.ui.demo.db.controller.App", {
         formatMail : function (sFirstName, sLastName) {
             var oBundle = this.getView().getModel("i18n").getResourceBundle()
 
-            return mobil
+            return mobileLibrary.URLHelper.normalizeEmail(
+                sFirstName + "." + sLastName + "@example.com",
+                oBundle.getText("mailSubject", [sFirstName]),
+                oBundle.getText("mailBody")
+            )
         }
     })
 });
