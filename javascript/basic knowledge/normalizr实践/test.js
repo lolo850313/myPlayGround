a62f6b68ea16d6772da0054e89818dcdc8881bf0
lolo <1542441232@qq.com>
2021-05-16 14:40:24 +0800

    123

--- javascript/basic knowledge/normalizr实践/test.js
+++ javascript/basic knowledge/normalizr实践/test.js
@@ -38,3 +38,24 @@ const article = new schema.Entity('article', {
 const normalizedData = normalize(originalData, article)
 console.log(normalizedData)
 
+// {
+//   result: "123",
+//   entities: {
+//     "articles": {
+//       "123": {
+//         id: "123",
+//         author: "1",
+//         title: "My awesome blog post",
+//         comments: [ "324" ]
+//       }
+//     },
+//     "users": {
+//       "1": { "id": "1", "name": "Paul" },
+//       "2": { "id": "2", "name": "Nicole" }
+//     },
+//     "comments": {
+//       "324": { id: "324", "commenter": "2" }
+//     }
+//   }
+// }
+
