16fad1883d3678925ba0b5c3676dea7629054108
lolo <1542441232@qq.com>
2021-05-17 18:03:57 +0800

    db15 buged

---
+++ javascript/openui5/practice/data binding/db15/webapp/index.js
@@ -0,0 +1,51 @@
+sap.ui.require([
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
+    "use strict";
+
+    sap.ui.getCore().attachInit(function () {
+        var oProductModel = new JSONModel()
+
+        oProductModel.loadData("./model/Products.json")
+        sap.ui.getCore().setModel(oProductModel, "products")
+
+        var oModel = new JSONModel({
+            firstName : "Harry",
+            lastName : "Hawk",
+            enabled: true,
+            address: {
+                street: "Dietmar-Hopp-Allee 16",
+                city: "Walldorf",
+                zip: "69190",
+                country: "Germany"
+                },
+            salesAmount: 12345.67,
+            currencyCode: "EUR",
+            priceThreshold : 20
+        })
+
+        sap.ui.getCore().setModel(oModel)
+
+        var oResourceModel = new ResourceModel({
+            bundleName : "sap.ui.demo.db.i18n.i18n",
+            supportedLocales : ["", "de"],
+            fallbackLocale : ""
+        })
+
+        sap.ui.getCore().setModel(oResourceModel, "i18n")
+
+        var oView = new XMLView({
+            viewName : "sap.ui.demo.db.view.App"
+        })
+
+        sap.ui.getCore().getMessageManager().registerObject(oView, true)
+        
+        // new XMLView({
+        //     viewName : "sap.ui.demo.db.view.App"
+        // }).placeAt("content")
+
+        oView.placeAt("content")
+    })
+})
